982998371f71e498adb23d53d633b5df116f9b04
morglod <morglod@gmail.com>
2018-11-07 16:04:24 +0300

    example upd

--- src/example.ts
+++ src/example.ts
@@ -2,6 +2,16 @@ import { EventEmitter } from './index';
 
 const events = new EventEmitter<{
     foo: (a: number, b: string) => void,
+    boo: (a: string) => void,
 }>();
 
-events.emit('foo', 123, 'helo');
+events.on('foo', (a, b) => {
+    console.log('foo:', a, b);
+});
+
+events.on('boo', (a) => {
+    console.log('boo:', a);
+});
+
+events.emit('foo', 123, 'hello');
+events.emit('boo', 'hello');
